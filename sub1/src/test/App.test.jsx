018210d6c00a64ccc5d482687b3cd170c43159c1
Re-Make-Re-Model <1992liumouren@gmail.com>
2023-05-17 03:41:34 +0800

    test sub1

--- sub1/src/test/App.test.jsx
+++ sub1/src/test/App.test.jsx
@@ -5,7 +5,7 @@ import App from '../App.jsx';
 describe('App tests', () => {
 	it('should contains the heading 1', () => {
 		render(<App />);
-		const heading = screen.getByText(/Sub1/i);
+		const heading = screen.getByText(/Sub 1/i);
 		expect(heading).toBeInTheDocument();
 	});
 });
