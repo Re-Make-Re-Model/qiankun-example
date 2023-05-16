7e6ea9eedf334cb524cc2efd06ec0f48dac10440
Re-Make-Re-Model <1992liumouren@gmail.com>
2023-05-17 03:40:48 +0800

    test home

--- home/src/test/App.test.jsx
+++ home/src/test/App.test.jsx
@@ -5,7 +5,7 @@ import App from '../App.jsx';
 describe('App tests', () => {
 	it('should contains the heading 1', () => {
 		render(<App />);
-		const heading = screen.getByText(/QianKun Home/i);
+		const heading = screen.getByText(/QianKun  Home/i);
 		expect(heading).toBeInTheDocument();
 	});
 });
